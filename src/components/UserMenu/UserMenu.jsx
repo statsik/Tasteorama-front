a26ff3d5b25f6085e4c7baabc8513202f87d10e8
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-09-15 23:17:20 +0200

    Icons-sprite-add

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,14 @@
+import css from './UserMenu.module.css';
+import Container from '../Container/Container';
+
+const UserMenu = () => {
+  return (
+    <div className={css["user-menu-wrapper"]}>
+      <Container>
+
+      </Container>
+    </div>
+  )
+}
+
+export default UserMenu;
