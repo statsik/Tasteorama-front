ece43814194878f1f642b2a3bf1c15a3d86efa52
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-09-16 18:21:37 +0200

    Navigation add

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,27 @@
+import clsx from 'clsx';
 import css from './Navigation.module.css';
+import { NavLink, Link } from 'react-router-dom';
+import UserMenu from '../UserMenu/UserMenu';
+import AuthNav from '../AuthNav/AuthNav';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
 const Navigation = () => {
-    return (
-        <></>
-    )
-}
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return (
+    <nav className={clsx(css["nav"])}>
+        <Link to="/">
+            <img src="/logo.svg" alt="Logo" className={clsx(css["logo"])} />
+        </Link>
+        <NavLink to='/recipes' className={clsx(css["recipes-nav"])}>
+            Recipes
+        </NavLink>
+      <div className={clsx(css.linkUserMenuAuthNavContainer)}>
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
