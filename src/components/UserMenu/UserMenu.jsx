cfa3e73dd22b71c26df0d87d07f157817e223504
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-09-16 18:20:37 +0200

    UserMenu add

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,14 +1,65 @@
-import css from './UserMenu.module.css';
-import Container from '../Container/Container';
+import clsx from 'clsx';
+import s from './UserMenu.module.css';
+import { NavLink } from 'react-router-dom';
+import { openModal } from '../../redux/modal/slice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from '../../redux/auth/selectors';
+
+const handleActiveClass = ({ isActive }) => {
+  return clsx(s.navLink, isActive && s.active);
+};
 
 const UserMenu = () => {
-  return (
-    <div className={css["user-menu-wrapper"]}>
-      <Container>
+  const { name, avatarUrl } = useSelector(selectUser);
 
-      </Container>
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+
+  const handleOpenConfirmExitModal = () => {
+    dispatch(openModal({ type: 'modalLogoutConfirm' }));
+  };
+
+  return (
+    <>
+      <NavLink to="/profile" className={handleActiveClass}>
+        My Profile
+      </NavLink>
+      <NavLink to="/create" className={clsx(s.createLink)}>
+        Add Recipe
+      </NavLink>
+
+      <div className={clsx(s.avatarContainer)}>
+        <div className={clsx(s.avatarNameContainer)}>
+          {avatarUrl ? (
+            <img className={s.avatar} src={avatarUrl} alt={name} />
+          ) : (
+            <div className={s.fallbackAvatar}>
+              {name
+                ?.split(' ')
+                .map((n) => n[0])
+                .join('')
+                .toUpperCase()}
+            </div>
+          )}
+          <p className={clsx(s.name)}>{name}</p>
+        </div>
+        <button
+          onClick={handleOpenConfirmExitModal}
+          type="button"
+          className={clsx(s.exitButton)}
+          aria-label="Exit from account"
+        >
+          <svg
+            width={24}
+            height={24}
+            className={clsx(s.exitIcon)}
+            aria-hidden="true"
+          >
+            <use href="/icons.svg#icon-exit"></use>
+          </svg>
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default UserMenu;
