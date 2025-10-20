89bd6126f1b288e897de7c2a59e1810a30747294
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-10-20 21:40:12 +0200

    NotFoundMatch-add

--- src/pages/NotFoundMatcPage/NotFoundMatcPage.jsx
+++ src/pages/NotFoundMatcPage/NotFoundMatcPage.jsx
@@ -1,7 +1,12 @@
-import './NotFoundMatcPage.module.css';
+import css from './NotFoundMatcPage.module.css';
 
 const NotFoundMatcPage = () => {
-    return(<></>)
+    return (
+        <div className={css["not-match-wrapper"]}>
+            <p className={css["not-match-title"]}>We’re sorry! We were not able to find a match.</p>
+            <button className={css["not-match-reset-button"]}>Reset serach and filters</button>
+        </div>
+    )
 }
 
 export default NotFoundMatcPage;
