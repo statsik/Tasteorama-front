6a76a4e5879aa3dc126245016db7db5763cb3c9b
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-10-21 19:08:51 +0200

    Error-saving-page-add

--- src/pages/ErrorSavingPage/ErrorSavingPage.jsx
+++ src/pages/ErrorSavingPage/ErrorSavingPage.jsx
@@ -1,9 +1,16 @@
-import './ErrorSavingPage.module.css';
+import css from './ErrorSavingPage.module.css';
 
 const ErrorSavingPage = () => {
-    return ({
-        
-    })
+    return (
+        <div className={css["error-saving-wrapper"]}>
+            <p className={css["error-saving-title"]}>Error while saving</p>
+            <p className={css["error-saving-description"]}>To save this recipe, you need to authorize first</p>
+            <div className={css["error-saving-button-wrapper"]}>
+                <button className={css["error-saving-button-login"]}>Log in</button>
+                <button className={css["error-saving-button-register"]}>Register</button>
+            </div>
+        </div>
+    )
 }
 
 export default ErrorSavingPage;
