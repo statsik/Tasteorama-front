48d32040bb3fd192f57b3eb48172951b296ea01f
Daniil Statsenko <daniilstatsenko23@gmail.com>
2025-10-22 21:18:24 +0200

    Sure-mode-page-add

--- src/pages/SureModPage/SureModPage.jsx
+++ src/pages/SureModPage/SureModPage.jsx
@@ -1,7 +1,15 @@
-import './SureModPage.module.css';
+import css from './SureModPage.module.css';
 
 const SureModPage = () => {
-    return(<></>)
+    return(
+    <div className={css["sure-mode-wrapper"]}>
+        <p className={css["sure-mode-title"]}>Are you shure?</p>
+        <p className={css["sure-mode-description"]}>We will miss you!</p>
+        <div className={css["sure-mode-button-wrapper"]}>
+            <button className={css["sure-mode-button-login"]}>Log out</button>
+            <button className={css["sure-mode-button-register"]}>Cancel</button>
+        </div>
+    </div>)
 }
 
 export default SureModPage;
